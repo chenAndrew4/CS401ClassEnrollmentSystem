94fcb0cfc0bd4358cb76c83e8f9e1182545a26da
Connor McMillan <connor@mcmillan.website>
2024-11-28 20:09:14 -0800

    Update BaseDashboardGUI.java
    Fixed a bug where the profile picture appear in the encapsulated box behind the opaque circle.

--- src/client/gui/dashboard/BaseDashboardGUI.java
+++ src/client/gui/dashboard/BaseDashboardGUI.java
@@ -122,7 +122,6 @@ public abstract class BaseDashboardGUI extends JFrame implements Runnable {
         personalImageLabel = new JLabel() {
             @Override
             protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
                 Graphics2D g2d = (Graphics2D) g.create();
                 int width = getWidth();
                 int height = getHeight();
