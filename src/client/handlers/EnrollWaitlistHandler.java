36ffa3fac5594254e3fb048c5d5fba2330debf76
A-Kungo <akungo@horizon.csueastbay.edu>
2024-12-02 12:07:22 -0800

    Please tell me if there's any mistakes.

--- src/client/handlers/EnrollWaitlistHandler.java
+++ src/client/handlers/EnrollWaitlistHandler.java
@@ -16,20 +16,20 @@ import shared.models.requests.EnrollWaitlistRequest;
 import shared.models.responses.EnrollWaitlistResponse;
 
 public class EnrollWaitlistHandler {
-	public void handleStudentEnroll(Student student, Course course, String sectionId, JPanel parentGUI, final BaseDashboardGUI parentDashboard, String sessionToken) 
+	public void handleStudentEnrollWaitlist(Student student, Course course, String sectionId, JPanel parentGUI, final BaseDashboardGUI parentDashboard, String sessionToken) 
 	{
 		EnrollWaitlistRequest enrollWaitlistRequest = new EnrollWaitlistRequest(MessageType.ENROLL_WAITLIST, null, sessionToken, course.getCourseID(), sectionId, student);
 		Client.getInstance().sendRequest(enrollWaitlistRequest, new ResponseCallback<EnrollWaitlistResponse, Void>() {
             @Override
-            public Void onSuccess(EnrollWaitlistResponse enrollCourseResponse) {
-            	JOptionPane.showMessageDialog(parentGUI, "Student has successfully been enrolled into " + course.getCourseID() + " Section: " + sectionId, "Enroll Course", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(ClientConfig.SUCCESS_ICON));
+            public Void onSuccess(EnrollWaitlistResponse enrollWaitlistResponse) {
+            	JOptionPane.showMessageDialog(parentGUI, "Student has successfully been enrolled into waitlist for" + course.getCourseID() + " Section: " + sectionId, "Enroll Course", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(ClientConfig.SUCCESS_ICON));
                 return null;
             }
 
             @Override
             public void onFailure(String reason) {
                 SwingUtilities.invokeLater(() -> {
-                	JOptionPane.showMessageDialog(parentGUI, "Could not be enrolled: " + reason, "Enroll Course Error", JOptionPane.ERROR_MESSAGE, new ImageIcon(ClientConfig.ERROR_ICON));
+                	JOptionPane.showMessageDialog(parentGUI, "Could not be enrolled: " + reason, "Enroll Waitlist Error", JOptionPane.ERROR_MESSAGE, new ImageIcon(ClientConfig.ERROR_ICON));
                 });
             }
         });
