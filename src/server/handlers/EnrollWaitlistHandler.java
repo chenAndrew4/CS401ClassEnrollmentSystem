ec1e03bc5239cc0ff0aaae7391a79f6ffa365b68
A-Kungo <akungo@horizon.csueastbay.edu>
2024-12-02 11:54:01 -0800

    Please tell me if there's any mistakes.

--- src/server/handlers/EnrollWaitlistHandler.java
+++ src/server/handlers/EnrollWaitlistHandler.java
@@ -12,22 +12,22 @@ import shared.models.responses.EnrollWaitlistResponse;
 public class EnrollWaitlistHandler {
     public static EnrollWaitlistResponse handleEnrollCourse(BaseRequest request, CourseService courseService, SessionService sessionService) 
     {
-        EnrollWaitlistRequest enrollCourseRequest = (EnrollWaitlistRequest) request;
-        if (sessionService.validateSession(enrollCourseRequest.getStudent().getUserId(), enrollCourseRequest.getSessionToken()))
+        EnrollWaitlistRequest enrollWaitlistRequest = (EnrollWaitlistRequest) request;
+        if (sessionService.validateSession(enrollWaitlistRequest.getStudent().getUserId(), enrollWaitlistRequest.getSessionToken()))
         {
-        	boolean success = courseService.enrollInCourse(enrollCourseRequest.getStudent(), enrollCourseRequest.getSectionId());
+        	boolean success = courseService.enrollInCourse(enrollWaitlistRequest.getStudent(), enrollWaitlistRequest.getSectionId());
         	if (success ) 
         	{
-                return new EnrollWaitlistResponse("enrolled", MessageStatus.SUCCESS, MessageType.ENROLL_WAITLIST, enrollCourseRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollCourseRequest.getStudent().getInstitutionID(), enrollCourseRequest.getSectionId(), enrollCourseRequest.getStudent()));
+                return new EnrollWaitlistResponse("enrolled", MessageStatus.SUCCESS, MessageType.ENROLL_WAITLIST, enrollWaitlistRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollWaitlistRequest.getStudent().getInstitutionID(), enrollWaitlistRequest.getSectionId(), enrollWaitlistRequest.getStudent()));
             } 
         	else 
         	{
-        		return new EnrollWaitlistResponse("not enrolled", MessageStatus.FAILURE, MessageType.ENROLL_WAITLIST, enrollCourseRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollCourseRequest.getStudent().getInstitutionID(), enrollCourseRequest.getSectionId(), enrollCourseRequest.getStudent()));
+        		return new EnrollWaitlistResponse("not enrolled", MessageStatus.FAILURE, MessageType.ENROLL_WAITLIST, enrollWaitlistRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollWaitlistRequest.getStudent().getInstitutionID(), enrollWaitlistRequest.getSectionId(), enrollWaitlistRequest.getStudent()));
             }
         } 
         else 
         {
-        	return new EnrollWaitlistResponse("Error", MessageStatus.FAILURE, MessageType.ENROLL_WAITLIST, enrollCourseRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollCourseRequest.getStudent().getInstitutionID(), enrollCourseRequest.getSectionId(), enrollCourseRequest.getStudent()));
+        	return new EnrollWaitlistResponse("Error", MessageStatus.FAILURE, MessageType.ENROLL_WAITLIST, enrollWaitlistRequest.getSectionId(), WaitlistService.getInstance().getWaitlistPositions(enrollWaitlistRequest.getStudent().getInstitutionID(), enrollWaitlistRequest.getSectionId(), enrollWaitlistRequest.getStudent()));
         }
     }
 }
